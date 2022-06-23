b885f48953f15d7b037cb277998ea2aa8f782184
maeek <maciej@suchanecki.me>
2022-06-23 23:49:24 +0200

    Increase limit

--- production/server/src/main.ts
+++ production/server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Response } from 'express';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,8 +13,10 @@ async function bootstrap() {
     next();
   });
 
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
   await app.listen(8080);
 }
 
-console.log('Starting server...');
 bootstrap();
